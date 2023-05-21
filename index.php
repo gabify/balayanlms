a79b1593a19d0473a75cd0dc0da119bf5575c685
gabify <ivangabriel.talaoc@g.batstate-u.edu.ph>
2023-05-21 12:30:21 +0800

    Add navbar

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php require '../balayanlms/template/header.php';?>
-    <h1>Library Management System</h1>
+    
 <?php require '../balayanlms/template/footer.php';?>
--- template/header.php
+++ template/header.php
@@ -4,6 +4,16 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="../balayanlms/node_modules/bootstrap/dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="../balayanlms/node_modules/bootstrap-icons/font/bootstrap-icons.min.css">
+        <link rel="stylesheet" href="../balayanlms/template/style.css">
         <title>Library Management System</title>
     </head>
     <body>
+        <nav class="navbar">
+            <div class="container-fluid px-4 py-2">
+                <a class="navbar-brand fs-4 fw-bold text-light" href="#">
+                <i class="bi-journal-text"></i>
+                Library Management System
+                </a>
+            </div>
+        </nav>
