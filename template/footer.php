abd649305493cb9ba358e19b1551d7fc82dbd339
gabify <ivangabriel.talaoc@g.batstate-u.edu.ph>
2023-05-21 08:56:27 +0800

    Initial Commit

---
+++ template/footer.php
@@ -0,0 +1,4 @@
+    <script src="../balayanlms/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="../balayanlms/node_modules/sweetalert2/dist/sweetalert2.all.min.js"></script>
+    </body>
+</html>
